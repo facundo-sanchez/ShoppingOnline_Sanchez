ab1fbd4b5796d539eeef7a806a564cf7a71c6d6b
Facundo Sanchez <facuujos@gmail.com>
2022-02-05 16:09:50 -0300

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
 import './components/styles/App.css';
-
-import Header from './components/layout/header/Header';
-import Footer from './components/layout/footer/Footer';
-
-import Main from './components/view/Home';
-import { Fragment ,useState,useEffect} from 'react';
 import Routes from './components/routes/Routes.';
 
 export default function App() {
   
-
-
-
-    return (
-
-    <Fragment>
-
-      <Routes
-      //  countFavorites = {countFavorites}
-      //  setCountFavorites={setCountFavorites} 
-      //  countBuys={countBuys} 
-      //  setCountBuys={setCountBuys}
-      //  selectItem = {selectItem}
-      //  setSelectItem ={setSelectItem}
-       />
-      {/* <Main/> */}
-
-    </Fragment>
+  return (
+    <Routes/>
   );
 }
-/*
- {selectItem &&(
-        <ItemDetailsContainer 
-        selectItem = {selectItem}
-        />  
-      )}
-    
-
-      <ItemListConteiner 
-       countFavorites = {countFavorites}
-       setCountFavorites={setCountFavorites} 
-       countBuys={countBuys} 
-       setCountBuys={setCountBuys}
-       setSelectItem ={setSelectItem}
-      />
-    */
